cc1ca13ee5ce1344e67cea5011b6a176aad0b01b
Davidson Nascimento <davidsonsns@gmail.com>
2019-05-09 20:29:13 -0300

    user schema timestamps

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -3,6 +3,6 @@ import { model, Schema } from 'mongoose'
 const schema = new Schema({
   firstName: { type: String, required: true },
   lastName: String,
-}, { minimize: true })
+}, { minimize: true, timestamps: true })
 
 export default model('User', schema)
